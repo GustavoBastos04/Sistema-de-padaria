ec08a641142ffe259c6cbc05c3a9f034ea045653
GustavoBastos04 <gubasouza2004@gmail.com>
2024-11-04 08:42:34 -0300

    Revert "Correct PostgreSQL"
    This reverts commit 0b7c14d32d928c22f77cc1b5b9c8feb2830ef045.

--- src/bakery/src/main/java/com/example/bakery/dtos/ClienteDTO.java
+++ src/bakery/src/main/java/com/example/bakery/dtos/ClienteDTO.java
@@ -3,18 +3,15 @@ package com.example.bakery.dtos;
 import com.example.bakery.entities.Cliente;
 
 // DTO = Data Transfer Object
-public class ClienteDTO {
-    private String cpf;
-    private String nome;
-    private String tipo_de_assinatura;
-    private String telefone;
-    private String email;
-    private String logradouro;
-    private String numero;
-    private String cep;
-    private String cidade;
-    private String uf;
+public record ClienteDTO(String cpf, String nome, String tipo_de_assinatura,
+                         String telefone, String email, String logradouro,
+                         String numero, String cep, String cidade, String uf) {
+
+    public ClienteDTO(Cliente cliente) {
+        this(cliente.getCpf(), cliente.getNome(), cliente.getTipo_de_assinatura(),
+                cliente.getTelefone(), cliente.getEmail(), cliente.getLogradouro(),
+                cliente.getNumero(), cliente.getCep(), cliente.getCidade(), cliente.getUf());
+    }
 
 
-    
 }
