12ba89b0235de0c0a099d02f77f409efb84bdc73
Kaique de Jesus Pessoa Santos <kaique10defevereiro@gmail.com>
2024-11-19 22:35:06 -0300

    Alteração nas consultas
    Precisei mudar o nome das chamadas de Padaria para padaria, pq meu schema estava sendo criado com padaria e não Padaria

--- sql/Consultas.sql
+++ sql/Consultas.sql
@@ -1,18 +1,19 @@
-SET search_path TO Padaria, public;
+SET search_path TO padaria;
 
 -- Desconto para Cliente Cadastrado
+-- Adcionei COALESCENCE para evitar valores nulos
 SELECT 
     c.nome,
     c.cpf,
-    SUM(v.valor) AS total_gasto,                            -- mostra o total gasto antes do desconto
+    ROUND(COALESCE(SUM(v.valor),0),2) AS total_gasto,                            -- mostra o total gasto antes do desconto
     CASE 
-        WHEN c.tipo_de_assinatura = 'Premium' THEN SUM(v.valor) * 0.10  -- desconto de 10% para clientes Premium
-        WHEN c.tipo_de_assinatura = 'Basic' THEN SUM(v.valor) * 0.05    -- desconto de 5% para clientes Basic
+        WHEN c.tipo_de_assinatura = 'Premium' THEN COALESCE(SUM(v.valor), 0) * 0.10  -- desconto de 10% para clientes Premium
+        WHEN c.tipo_de_assinatura = 'Basic' THEN COALESCE(SUM(v.valor), 0) * 0.05    -- desconto de 5% para clientes Basic
         ELSE 0                                                          -- sem desconto para outros tipos
     END AS desconto, 
-    SUM(v.valor) - CASE 
-        WHEN c.tipo_de_assinatura = 'Premium' THEN SUM(v.valor) * 0.10  -- valor final após desconto de 10% para Premium
-        WHEN c.tipo_de_assinatura = 'Basic' THEN SUM(v.valor) * 0.05    -- valor final após desconto de 5% para Basic
+    ROUND(COALESCE(SUM(v.valor),0),2) - CASE 
+        WHEN c.tipo_de_assinatura = 'Premium' THEN COALESCE(SUM(v.valor), 0) * 0.10  -- valor final após desconto de 10% para Premium
+        WHEN c.tipo_de_assinatura = 'Basic' THEN COALESCE(SUM(v.valor), 0) * 0.05    -- valor final após desconto de 5% para Basic
         ELSE 0                                                          -- sem desconto para outros tipos
     END AS total_com_desconto
 FROM
@@ -106,50 +107,41 @@ LEFT JOIN
 --v.data_de_venda BETWEEN '2024-12-01' AND '2024-12-12'
 --Tem que colocar a data aqui, pensei em deixar uma variavel no lugar
 -- Lucro em determinado periodo
-DO $$ 
-DECLARE
-    data_inicial DATE := '2024-01-01';
-    data_final DATE := '2024-12-01';
-BEGIN
-    EXECUTE '
-	WITH CustoVenda as (
-        SELECT 
-            iv.venda_id_venda,
-            SUM(fie.preco * pci.quantidade) as custo_total_ingredientes
-        FROM 
-            Padaria.item_venda as iv
-        JOIN 
-            Padaria.produto_constituido_ingrediente as pci on iv.produto_id_produto = pci.produto_id_produto
-        JOIN 
-            Padaria.ingrediente as i on pci.ingrediente_id_ingrediente = i.id_ingrediente
-        JOIN 
-            Padaria.fornece_item_estoque as fie on i.estoque_id_item = fie.estoque_id_item
-        GROUP BY 
-            iv.venda_id_venda
-    ), Receita_Periodo as (
-        SELECT
-            v.id_venda,
-            v.valor,
-            v.data_de_venda
-        FROM
-            Padaria.venda as v
-        WHERE 
-            v.data_de_venda BETWEEN' || quote_literal(data_inicial) || 'AND' || quote_literal(data_final) ||'
-    ) 	
+WITH CustoVenda AS (
+    SELECT 
+        iv.venda_id_venda,
+        SUM(fie.preco * pci.quantidade) AS custo_total_ingredientes
+    FROM 
+        Padaria.item_venda AS iv
+    JOIN 
+        Padaria.produto_constituido_ingrediente AS pci ON iv.produto_id_produto = pci.produto_id_produto
+    JOIN 
+        Padaria.ingrediente AS i ON pci.ingrediente_id_ingrediente = i.id_ingrediente
+    JOIN 
+        Padaria.fornece_item_estoque AS fie ON i.estoque_id_item = fie.estoque_id_item
+    GROUP BY 
+        iv.venda_id_venda
+), Receita_Periodo AS (
     SELECT
-		-- Exibir o período selecionado
-		''' || data_inicial || ''' AS Data_Inicial,  -- Exibe a data inicial
-        ''' || data_final || ''' AS Data_Final,    -- Exibe a data final
-        rep.id_venda,
-        rep.valor as receita,
-        COALESCE(cv.custo_total_ingredientes, 0) as custo,
-        rep.valor - COALESCE(cv.custo_total_ingredientes, 0) as lucro
+        v.id_venda,
+        v.valor,
+        v.data_de_venda
     FROM
-        Receita_Periodo as rep
-    LEFT JOIN
-        CustoVenda as cv on rep.id_venda = cv.venda_id_venda';
-END $$;
-
+        Padaria.venda AS v
+    WHERE 
+        v.data_de_venda BETWEEN '2024-01-01' AND '2024-12-01'
+) 	
+SELECT
+    '2024-01-01' AS Data_Inicial,  -- Exibe a data inicial
+    '2024-12-01' AS Data_Final,    -- Exibe a data final
+    ROUND(SUM(rep.valor), 2) AS receita_total,
+    ROUND(SUM(COALESCE(cv.custo_total_ingredientes, 0)), 2) AS custo_total,
+    ROUND(SUM(rep.valor) - SUM(COALESCE(cv.custo_total_ingredientes, 0)), 2) AS lucro_total
+FROM
+    Receita_Periodo AS rep
+LEFT JOIN
+    CustoVenda AS cv ON rep.id_venda = cv.venda_id_venda;
+	
 -- Relatorio de meios de pagamento
 -- Quantidade de vendas por tipo de pagamento e valor total das vendas
 SELECT p.tipo, count(p.tipo) AS qtd_vendas, Sum(v.valor) AS valor_total
