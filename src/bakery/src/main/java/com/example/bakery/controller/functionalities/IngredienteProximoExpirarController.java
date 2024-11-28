a9b58760dc26ff82367fade2b72cfd84274f2f89
gustavohnsv <guvi10vieira@gmail.com>
2024-11-28 09:10:37 -0300

    fix: fixed filename

--- src/bakery/src/main/java/com/example/bakery/controller/functionalities/IngredienteProximoExpirarController.java
+++ src/bakery/src/main/java/com/example/bakery/controller/functionalities/IngredienteProximoExpirarController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("padaria")
-public class CustomIngredienteProximoExpirarController {
+public class IngredienteProximoExpirarController {
 
     @Autowired
     IngredienteRepository ingredienteRepository;
